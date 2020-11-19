5000adb97964a9427625f178c3842167638f085a
Rasmus Cederdorff <rasmus@cederdorff.com>
2020-11-19 08:21:08 +0100

    vue projects added

---
+++ vue-cli-fire-posts/src/views/Home.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="home">
+    <h1>Vue Fire Post App</h1>
+    <post-list></post-list>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import PostList from "../components/PostList.vue";
+export default {
+  name: "Home",
+  components: {
+    PostList
+  }
+};
+</script>
